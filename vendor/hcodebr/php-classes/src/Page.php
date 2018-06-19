8c30363a47b5561e03e4b8ef189bd598a2f6da24
Derik Nyvo <derikoliveiranyhc@hotmail.com>
2018-06-19 17:04:01 -0300

    Criando PageAdmin e o TPLAdmin

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -12,12 +12,12 @@ class Page
         'data' => []
     ];
 
-    public function __construct($opts = [])
+    public function __construct($opts = [], $tpl_dir = '/views/')
     {
         $this->options = array_merge($this->default, $opts);
 
         $config = [
-            "tpl_dir"       => $_SERVER['DOCUMENT_ROOT'] . '/views/',
+            "tpl_dir"       => $_SERVER['DOCUMENT_ROOT'] . $tpl_dir,
             "cache_dir"     => $_SERVER['DOCUMENT_ROOT'] . '/views-cache/',
             "debug"         => false
         ];
---
+++ vendor/hcodebr/php-classes/src/PageAdmin.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Hcode;
+
+class PageAdmin extends Page
+{
+    public function __construct($opts = [], $tpl_dir = '/views/admin/')
+    {
+        parent::__construct($opts, $tpl_dir);
+    }
+}
