cd8a476eefa53e74acb41df641e10e4981b403ea
Derik Nyvo <derikoliveiranyhc@hotmail.com>
2018-08-04 09:59:56 -0300

    categorias de produtos

---
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Hcode\Model;
+
+use \Hcode\DB\Sql;
+use \Hcode\Model;
+
+class Category extends Model
+{
+    public static function listAll()
+    {
+        $sql = new Sql();
+
+        return $sql->select('SELECT * FROM tb_categories');
+    }
+
+    public function save()
+    {
+        $sql = new Sql();
+
+        $results = $sql->select('CALL sp_categories_save(:idcategory, :descategory)', [
+            ':idcategory' => $this->getidcategory(),
+            ':descategory' => $this->getdescategory()
+        ]);
+
+        $this->setData($results[0]);
+    }
+
+    public function get($idcategory)
+    {
+        $sql = new Sql();
+
+        $results = $sql->select('SELECT * FROM tb_categories WHERE idcategory = :idcategory', [
+            ':idcategory' => $idcategory
+        ]);
+
+        $this->setData($results[0]);
+    }
+
+    public function delete()
+    {
+        $sql = new Sql();
+
+        $results = $sql->query('DELETE FROM tb_categories WHERE idcategory = :idcategory', [
+            ':idcategory' => $this->getidcategory()
+        ]);
+    }
+}
