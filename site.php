36e1c246f2db38beaaba1d1f0378baf6a8b58b3e
Derik Nyvo <derikoliveiranyhc@hotmail.com>
2018-08-07 18:02:51 -0300

    crud de produtos

---
+++ site.php
@@ -0,0 +1,10 @@
+<?php
+
+use \Hcode\Page;
+
+$app->get('/', function() {
+
+    $page = new Page();
+
+    $page->setTpl('index');
+});
---
+++ sp_products_save.sql
@@ -0,0 +1,45 @@
+USE `db_ecommerce`;
+DROP procedure IF EXISTS `sp_products_save`;
+
+DELIMITER $$
+USE `db_ecommerce`$$
+CREATE PROCEDURE `sp_products_save`(
+pidproduct int(11),
+pdesproduct varchar(64),
+pvlprice decimal(10,2),
+pvlwidth decimal(10,2),
+pvlheight decimal(10,2),
+pvllength decimal(10,2),
+pvlweight decimal(10,2),
+pdesurl varchar(128)
+)
+BEGIN
+	
+	IF pidproduct > 0 THEN
+		
+		UPDATE tb_products
+        SET 
+			desproduct = pdesproduct,
+            vlprice = pvlprice,
+            vlwidth = pvlwidth,
+            vlheight = pvlheight,
+            vllength = pvllength,
+            vlweight = pvlweight,
+            desurl = pdesurl
+        WHERE idproduct = pidproduct;
+        
+    ELSE
+		
+		INSERT INTO tb_products (desproduct, vlprice, vlwidth, vlheight, vllength, vlweight, desurl) 
+        VALUES(pdesproduct, pvlprice, pvlwidth, pvlheight, pvllength, pvlweight, pdesurl);
+        
+        SET pidproduct = LAST_INSERT_ID();
+        
+    END IF;
+    
+    SELECT * FROM tb_products WHERE idproduct = pidproduct;
+    
+END$$
+
+DELIMITER ;
+
