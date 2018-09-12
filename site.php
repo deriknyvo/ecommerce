ee4bd031f49a50e11369211aa34049339001b0cb
Derik Nyvo <derikoliveiranyhc@hotmail.com>
2018-09-12 16:48:36 -0300

    validando carrinho de compra

--- site.php
+++ site.php
@@ -3,8 +3,9 @@
 use \Hcode\Page;
 use \Hcode\Model\Product;
 use \Hcode\Model\Category;
+use \Hcode\Model\Cart;
 
-$app->get('/', function() {
+$app->get('/', function () {
 
     $products = Product::listAll();
 
@@ -15,16 +16,52 @@ $app->get('/', function() {
     ]);
 });
 
-$app->get('/categories/:idcategory', function($idcategory) {
+$app->get('/categories/:idcategory', function ($idcategory) {
+
+    $page = isset($_GET['page']) ? (int) $_GET['page'] : 1;
 
     $category = new Category();
 
     $category->get((int) $idcategory);
+    $pagination = $category->getProductsPage($page);
+
+    $pages = [];
+
+    for ($i = 1; $i <= $pagination['pages']; $i++) {
+        array_push($pages, [
+            'link' => '/categories/' . $category->getidcategory() . '?page=' . $i,
+            'page' => $i
+        ]);
+    }
 
     $page = new Page();
 
     $page->setTpl('category', [
         'category' => $category->getValues(),
-        'products' => Product::checkList($category->getProducts())
+        'products' => $pagination['data'],
+        'pages' => $pages
     ]);
+});
+
+$app->get('/products/:desurl', function ($desurl) {
+
+    $product = new Product();
+
+    $product->getFromURL($desurl);
+
+    $page = new Page();
+
+    $page->setTpl('product-detail', [
+        'product' => $product->getValues(),
+        'categories' => $product->getCategories()
+    ]);
+});
+
+$app->get('/cart', function () {
+
+    $cart = Cart::getFromSession();
+
+    $page = new Page();
+
+    $page->setTpl('cart');
 });
--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -104,4 +104,27 @@ class Category extends Model
             ':idproduct' => $product->getidproduct()
         ]);
     }
+
+    public function getProductsPage($page = 1, $itemsPerPage = 3)
+    {
+        $start = ($page - 1) * $itemsPerPage;
+
+        $sql = new Sql();
+
+        $results = $sql->select("SELECT SQL_CALC_FOUND_ROWS * FROM tb_products a
+            INNER JOIN tb_productscategories b ON a.idproduct = b.idproduct
+            INNER JOIN tb_categories c ON c.idcategory = b.idcategory
+            WHERE c.idcategory = :idcategory
+            LIMIT $start, $itemsPerPage", [
+                ':idcategory' => $this->getidcategory()
+            ]);
+
+        $resultTotal = $sql->select('SELECT FOUND_ROWS() as nrtotal');
+
+        return [
+            'data' => Product::checkList($results),
+            'total' => (int) $resultTotal[0]['nrtotal'],
+            'pages' => ceil($resultTotal[0]['nrtotal'] / $itemsPerPage)
+        ];
+    }
 }
